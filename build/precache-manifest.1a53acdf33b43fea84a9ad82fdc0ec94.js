f45ebbe606e1d41a50f38ea34da0cbc3ad735687
your_name <your_email@example.com>
2020-01-08 14:10:07 +0900

    build

---
+++ build/precache-manifest.1a53acdf33b43fea84a9ad82fdc0ec94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4047b0fbe1d8e221c642e74868a4c2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d83cb1292878112620e",
+    "url": "/static/css/main.7368be98.chunk.css"
+  },
+  {
+    "revision": "cfc7dbcd304da8383908",
+    "url": "/static/js/2.2f980e9b.chunk.js"
+  },
+  {
+    "revision": "0d83cb1292878112620e",
+    "url": "/static/js/main.ad80ef12.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
