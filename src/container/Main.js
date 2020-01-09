9c3dcd0e4c017004ca51ef7255a5eebe3d3ae58c
your_name <your_email@example.com>
2020-01-09 11:27:25 +0900

    Reload

--- src/container/Main.js
+++ src/container/Main.js
@@ -22,10 +22,10 @@ class Main extends Component {
 
           <div className="menu">
             <ul className="nav">
-              <li className="Home" id="moveBtn1">홈으로.</li>
-              <li className="myself" id="moveBtn2">자기소개</li>
-              <li className="Skill" id="moveBtn3">스킬</li>
-              <li className="project" id="moveBtn4">프로젝트</li>
+              <li className="Home" id="moveBtn1">Intro</li>
+              <li className="myself" id="moveBtn2">ABOUT</li>
+              <li className="Skill" id="moveBtn3">SKILL</li>
+              <li className="project" id="moveBtn4">PROJECT</li>
               <li className="contact" id="moveBtn5">CONTACT</li>
             </ul>
           </div>
@@ -55,21 +55,24 @@ class Main extends Component {
 
         <div id="preId11" class="">
           <p class="Basic"> Basic   </p> <br/>
-          
-          <Image src= "https://i.imgur.com/Hkl9ITj.png" width="190" height="190" class="Basici" title="C"/> 
-          <Image src= "https://i.imgur.com/NZHCD9r.png" width="190" height="190" class="Basici" title="C#"/> 
-          <Image src= "https://i.imgur.com/1hZMpis.png" width="190" height="190" class="Basici" title="JAVA"/> 
-          <Image src= "https://i.imgur.com/42snnxa.png" width="190" height="190" class="Basici" title="JavaScript"/>   
-          <Image src= "https://i.imgur.com/CYsmYzI.png" width="190" height="190" class="Basici" title="Window"/> 
-          <Image src= "https://i.imgur.com/Q4khk7A.png" width="190" height="190" class="Basici" title="Linux"/>
+          <div>
+            <Image src= "https://i.imgur.com/Hkl9ITj.png" width="190" height="190" class="Basici" title="C"/> 
+            <Image src= "https://i.imgur.com/NZHCD9r.png" width="190" height="190" class="Basici" title="C#"/> 
+            <Image src= "https://i.imgur.com/1hZMpis.png" width="190" height="190" class="Basici" title="JAVA"/> 
+            <Image src= "https://i.imgur.com/42snnxa.png" width="190" height="190" class="Basici" title="JavaScript"/>   
+            <Image src= "https://i.imgur.com/CYsmYzI.png" width="190" height="190" class="Basici" title="Window"/> 
+            <Image src= "https://i.imgur.com/Q4khk7A.png" width="190" height="190" class="Basici" title="Linux"/>
+          </div>
   
           <p class="Intermediate"> Intermediate </p>
-          <Image src= "https://i.imgur.com/lLFbSjn.png" width="190" height="190" class="Basici" title="JSP"/> 
-          <Image src= "https://i.imgur.com/ovYkpUz.png" width="190" height="190" class="Basici" title="Mysql"/> 
-          <Image src= "https://i.imgur.com/0HXrAwV.png" width="250" height="190" class="Basici" title="Unity"/> 
-          <Image src= "https://i.imgur.com/HHPQN5k.png" width="190" height="190" class="Basici" title="React"/>   
-          <Image src= "https://i.imgur.com/3byEipA.png" width="190" height="190" class="Basici" title="Github"/> 
-          <Image src= "https://i.imgur.com/5TgK2mc.png" width="250" height="190" class="Basici" title="gnuboard"/>
+          <div>
+            <Image src= "https://i.imgur.com/lLFbSjn.png" width="190" height="190" class="Basici" title="JSP"/> 
+            <Image src= "https://i.imgur.com/ovYkpUz.png" width="190" height="190" class="Basici" title="Mysql"/> 
+            <Image src= "https://i.imgur.com/0HXrAwV.png" width="250" height="190" class="Basici" title="Unity"/> 
+            <Image src= "https://i.imgur.com/HHPQN5k.png" width="190" height="190" class="Basici" title="React"/>   
+            <Image src= "https://i.imgur.com/3byEipA.png" width="190" height="190" class="Basici" title="Github"/> 
+            <Image src= "https://i.imgur.com/5TgK2mc.png" width="250" height="190" class="Basici" title="gnuboard"/>
+          </div>
             
         </div>
 
@@ -81,16 +84,17 @@ class Main extends Component {
               <div>
                 <a href="https://github.com/imsocuty/Rock-paper-scissors"> <Image src=
                     "https://i.imgur.com/3Pk8K1U.jpg"
-                    align="left" vspace="30" hspace="40" width="500" height="400" /> </a>
+                     class="project_img"/> </a>
+                <div>
+                  <h1> 회사키우기 </h1>
 
-                  <br/><br/> <h1> 회사키우기 </h1> <br/>
+                  <p> 컴퓨터와 가위바위보를 하며 회사를 성장시키는 CUI 게임 </p> <br/>
 
-                <p> 컴퓨터와 가위바위보를 하며 회사를 성장시키는 CUI 게임 </p> <br/> 
-
-                <h2> 개발 기간 </h2> <br />
-                <p> 2017.11.25 ~ 2017.11.26 </p> <br />
-                <a href="https://github.com/imsocuty/Rock-paper-scissors"> 
-                <Image src= "https://i.imgur.com/3byEipA.png" class="github"/> </a>  
+                  <h2> 개발 기간 </h2>
+                  <p> 2017.11.25 ~ 2017.11.26 </p>
+                  <a href="https://github.com/imsocuty/Rock-paper-scissors"> 
+                  <Image src= "https://i.imgur.com/3byEipA.png" class="github"/> </a>  
+                </div>
               </div>
 
 
@@ -98,16 +102,17 @@ class Main extends Component {
               <div>
                 <a href="https://github.com/imsocuty/Movie"><Image src=
                   "https://i.imgur.com/Eu6rhlz.png"
-                  align="left" vspace="30" hspace="40" width="500" height="400" /> </a>
-              
-              <br/> <br/> <h1> 영화 관리 사이트 </h1> <br />
+                   class="project_img"/> </a>
+                <div>
+                  <h1> 영화 관리 사이트 </h1>
 
-                <p> JSP, MYSQL을 이용하여 로그인 기능이 구현된 영화 관리 사이트 </p> <br/> 
+                  <p> JSP, MYSQL을 이용하여 로그인 기능이 구현된 영화 관리 사이트 </p> <br/>
 
-                <h2> 개발 기간 </h2> <br />
-                <p> 2019.08.25 ~ 2019.08.25 </p> <br />
-                <a href="https://github.com/imsocuty/Movie"> 
-                <Image src= "https://i.imgur.com/3byEipA.png" class="github"/> </a>
+                  <h2> 개발 기간 </h2>
+                  <p> 2019.08.25 ~ 2019.08.25 </p>
+                  <a href="https://github.com/imsocuty/Movie"> 
+                  <Image src= "https://i.imgur.com/3byEipA.png" class="github"/> </a>
+                </div>
               </div>
             </div>
 
@@ -115,32 +120,35 @@ class Main extends Component {
               <div>
                 <a href="https://github.com/imsocuty/FPS"><Image src=
                   "https://i.imgur.com/v7OdOkS.png"
-                  align="left" vspace="30" hspace="40" width="500" height="400" /> </a>
+                  class="project_img"/> </a>
+                <div>
+                  <h1> 1인칭 FPS </h1>
 
-                <br/><br/> <h1> 1인칭 FPS </h1> <br />
+                  <p> 유니티를 이용하여 개발한 1인칭 FPS 게임 </p> <br/>
 
-                <p> 유니티를 이용하여 개발한 1인칭 FPS 게임 </p> <br/> 
+                  <h2> 개발 기간 </h2>
+                  <p> 2019.06.01 ~ 2019.06.30 </p>
+                  <a href="https://github.com/imsocuty/FPS"> 
 
-                <h2> 개발 기간 </h2> <br />
-                <p> 2019.06.01 ~ 2019.06.30 </p> <br />
-                <a href="https://github.com/imsocuty/FPS"> 
-                <Image src= "https://i.imgur.com/3byEipA.png" class="github"/> </a>
+                  <Image src= "https://i.imgur.com/3byEipA.png" class="github"/> </a>
+                </div>
               </div>
 
 
               <div>
                 <a href="https://github.com/imsocuty/TowerDefense"><Image src=
                   "https://i.imgur.com/OAOIiB0.png"
-                  align="left" vspace="30" hspace="40" width="500" height="400" /> </a>
+                  class="project_img"/> </a>
+                <div>
+                  <h1> 3D 타워 디펜스 </h1>
 
-                <br/><br/> <h1> 3D 타워 디펜스 </h1> <br />
+                  <p>  타워를 건설하여 지켜내는 3D 타워디펜스 게임 </p> <br/>
 
-                <p>  타워를 건설하여 지켜내는 3D 타워디펜스 게임 </p> <br/> 
-
-                <h2> 개발 기간 </h2> <br />
-                <p> 2019.12.16 ~ 2019.12.19 </p> <br />
-                <a href="https://github.com/imsocuty/TowerDefense"> 
-                <Image src= "https://i.imgur.com/3byEipA.png" class="github"/> </a>
+                  <h2> 개발 기간 </h2>
+                  <p> 2019.12.16 ~ 2019.12.19 </p>
+                  <a href="https://github.com/imsocuty/TowerDefense"> 
+                  <Image src= "https://i.imgur.com/3byEipA.png" class="github"/> </a>
+                </div>
               </div>
             </div>
           </div>
@@ -224,4 +232,12 @@ $(function(){
   $(this).addClass('on')})
 })
 
+$('html, body').css({'overflow': 'hidden'});
+$('#element').on('scroll touchmove mousewheel', function(event) {
+  return false;
+});
+
+$(document).ready(function() {
+  $("html,body").animate({scrollTop: 0}, 100); //100ms for example
+});
 export default Main;
