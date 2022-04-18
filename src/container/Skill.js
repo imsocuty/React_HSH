1d652cfb8c8c6de0e049d8d889f802aa98476cdf
IMSOSEXY <ppppp4710@naver.com>
2022-04-19 01:31:20 +0900

    Update

--- src/container/Skill.js
+++ src/container/Skill.js
@@ -12,7 +12,7 @@ class Skill extends Component {
             <div id="preId11" class="">
                 <p class="Basic"> Basic   </p> <br/>
                 <div>
-                    <Image src= "https://i.ibb.co/BPtdwR7/img.jpg" width="250" height="190" class="Basici" title="Spring"/>
+                    <Image src= "https://i.ibb.co/rfbYhCc/img-removebg-preview.png" width="250" height="190" class="Basici" title="Spring"/>
                     <Image src= "https://i.imgur.com/HHPQN5k.png" width="190" height="190" class="Basici" title="React"/>
                     <Image src= "https://i.imgur.com/NZHCD9r.png" width="190" height="190" class="Basici" title="C#"/>
                     <Image src= "https://i.imgur.com/0HXrAwV.png" width="250" height="190" class="Basici" title="Unity"/>
@@ -24,7 +24,7 @@ class Skill extends Component {
                 <div>
                     <Image src= "https://i.imgur.com/lLFbSjn.png" width="190" height="190" class="Basici" title="JSP"/>
                     <Image src= "https://i.imgur.com/ovYkpUz.png" width="190" height="190" class="Basici" title="Mysql"/>
-                    <Image src= "https://i.ibb.co/zN0F60F/images-removebg-preview.png" width="190" height="190" class="Basici" title="ORACLE"/>
+                    <Image src= "https://i.ibb.co/JC4kKLL/ORACLE-removebg-preview.png" width="190" height="190" class="Basici" title="ORACLE"/>
                     <Image src= "https://i.imgur.com/1hZMpis.png" width="190" height="190" class="Basici" title="JAVA"/>
                     <Image src= "https://i.imgur.com/42snnxa.png" width="190" height="190" class="Basici" title="JavaScript"/>
                     <Image src= "https://i.imgur.com/3byEipA.png" width="190" height="190" class="Basici" title="Github"/>
