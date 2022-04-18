7628f20c138b5832d92ae24a16ef030f4a99a26a
IMSOSEXY <ppppp4710@naver.com>
2022-04-19 01:42:05 +0900

    Update

--- src/container/About.js
+++ src/container/About.js
@@ -14,7 +14,7 @@ window.$ = window.jQuery = jQuery;
             <div class="jb-wrap">
   
               <div class="jb-image">
-                <Image src= "https://i.ibb.co/FXkqSSH/HSH.png"
+                <Image src= "https://i.ibb.co/gmqwgf1/HSH.jpg"
                   align="left" vspace="30" hspace="10" class="intro"/>
                 <p>  MAIL: <a href="mailto:pppp4710@naver.com" target="_black">pppp4710@naver.com</a> | BLOG : <a href="http://kingsman5.tistory.com" target="_black">kingsman5.tistory.com</a> </p>
               </div>
