61a7cb9a48ab61e4a42d3caf3f04d65278763337
IMSOSEXY <ppppp4710@naver.com>
2022-04-19 09:12:23 +0900

    Update

--- src/container/Skill.js
+++ src/container/Skill.js
@@ -12,7 +12,7 @@ class Skill extends Component {
             <div id="preId11" class="">
                 <p class="Basic"> Basic   </p> <br/>
                 <div>
-                    <Image src= "https://i.ibb.co/rfbYhCc/img-removebg-preview.png" width="250" height="190" class="Basici" title="Spring"/>
+                    <Image src= "https://i.ibb.co/1fqDpVM/SP-removebg-preview.png" width="250" height="190" class="Basici" title="Spring"/>
                     <Image src= "https://i.imgur.com/HHPQN5k.png" width="190" height="190" class="Basici" title="React"/>
                     <Image src= "https://i.imgur.com/NZHCD9r.png" width="190" height="190" class="Basici" title="C#"/>
                     <Image src= "https://i.imgur.com/0HXrAwV.png" width="250" height="190" class="Basici" title="Unity"/>
